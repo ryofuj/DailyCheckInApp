2664435c4db4d125da6689c949ec12530bfaf52e
Ryo Fuji <ryo176999@gmail.com>
2022-10-01 13:15:18 -0700

    xcode init

---
+++ Shared/DailyCheckInAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCheckInAppApp.swift
+//  Shared
+//
+//  Created by ryofuji on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyCheckInAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
